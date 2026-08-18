d1a5a005d2a79c2e380637871f32cc6a6b2b7cb3
gxc <1003823149@qq.com>
2026-08-18 14:07:26 +0800

    feat: fix jwt

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import profile, refresh_token, wechat_login
 
@@ -6,5 +7,7 @@ urlpatterns = [
     path("wechat-login/", wechat_login),
     path("token/refresh/", refresh_token),
     path("profile/", profile),
+    path("api/login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/refresh", TokenRefreshView.as_view(), name="token_refresh_pair"),
 ]
 
